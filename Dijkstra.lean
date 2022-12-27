70b2f49723ab3ae820d4663efb5a6818e99d1abf
Junology <jtachan@hotmail.com>
2022-12-27 16:13:35 +0900

    export DijkstraMonad.dpure and DijkstraMonad.dbind

--- Dijkstra.lean
+++ Dijkstra.lean
@@ -10,8 +10,10 @@ class DijkstraMonad (WP : Type u → Type v) [Monad WP] (M : {α : Type u} → W
   dpure {α : Type u} (a : α) : M (return a)
   dbind {α β : Type u} {wa : WP α} (c : M wa) {wf : α → WP β} (f : (a : α) → M (wf a)) : M (wa >>= wf)
 
+export DijkstraMonad (dpure dbind)
+
 class LawfulDijkstraMonad (WP : Type u → Type v) [Monad WP] (M : {α : Type u} → WP α → Type v) [DijkstraMonad WP M] where
-  dbind_dpure {α : Type u} {wa : WP α} {x : M wa} : DEq M (DijkstraMonad.dbind x DijkstraMonad.dpure) x
-  dpure_dbind {α β : Type u} (a : α) {wb : WP β} (wf : α → M wb) : DEq M (DijkstraMonad.dbind (DijkstraMonad.dpure a) wf) (wf a)
-  dbind_assoc {α β γ : Type u} {wa : WP α} {wb : WP β} {wc : WP γ} (x : M wa) (f : α → M wb) (g : β → M wc) : DEq M (DijkstraMonad.dbind (DijkstraMonad.dbind x f) g) (DijkstraMonad.dbind x (λ a => DijkstraMonad.dbind (f a) g))
+  dbind_dpure {α : Type u} {wa : WP α} {x : M wa} : DEq M (dbind x dpure) x
+  dpure_dbind {α β : Type u} (a : α) {wb : WP β} (wf : α → M wb) : DEq M (dbind (dpure a) wf) (wf a)
+  dbind_assoc {α β γ : Type u} {wa : WP α} {wb : WP β} {wc : WP γ} (x : M wa) (f : α → M wb) (g : β → M wc) : DEq M (dbind (dbind x f) g) (dbind x (λ a => dbind (f a) g))
 
