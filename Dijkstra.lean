145e00b1696f2880a5325df8f49001d27f7e2cb0
Junology <jtachan@hotmail.com>
2022-12-27 14:26:30 +0900

    Axioms for Dijkstra monads.

--- Dijkstra.lean
+++ Dijkstra.lean
@@ -5,11 +5,15 @@ import Dijkstra.Control.SpecMonad
 
 universe u v v₁ v₂ v₃ w
 
+inductive DEq {α : Type u} (β : α → Type v) : {a₁ a₂ : α} → β a₁ → β a₂ → Prop
+| rfl {a : α} (b : β a) : DEq β b b
 
-class DijkstraMonad {w : Type u → Type v} [Monad w] (mw : {α : Type u} → w α → Type v) where
-  pure {α : Type u} (a : α) : mw (return a)
-  bind {α β : Type u} {a : α} {wa : w α} (c : mw wa) {wf : α → w β} (f : (a : α) → mw (wf a)) : mw (wa >>= wf)
+class DijkstraMonad (WP : Type u → Type v) [Monad WP] (M : {α : Type u} → WP α → Type v) where
+  dpure {α : Type u} (a : α) : M (return a)
+  dbind {α β : Type u} {wa : WP α} (c : M wa) {wf : α → WP β} (f : (a : α) → M (wf a)) : M (wa >>= wf)
 
-def WriterT (m : Type (max u w) → Type v) (σ : Type w) (α : Type u) : Type v :=
-  m (σ × α)
+class LawfulDijkstraMonad (WP : Type u → Type v) [Monad WP] (M : {α : Type u} → WP α → Type v) [DijkstraMonad WP M] where
+  dbind_dpure {α : Type u} {wa : WP α} {x : M wa} : DEq M (DijkstraMonad.dbind x DijkstraMonad.dpure) x
+  dpure_dbind {α β : Type u} (a : α) {wb : WP β} (wf : α → M wb) : DEq M (DijkstraMonad.dbind (DijkstraMonad.dpure a) wf) (wf a)
+  dbind_assoc {α β γ : Type u} {wa : WP α} {wb : WP β} {wc : WP γ} (x : M wa) (f : α → M wb) (g : β → M wc) : DEq M (DijkstraMonad.dbind (DijkstraMonad.dbind x f) g) (DijkstraMonad.dbind x (λ a => DijkstraMonad.dbind (f a) g))
 
