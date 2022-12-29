85659560f52f49ee3a8c55cdcb9f70aa1294aac4
Junology <jtachan@hotmail.com>
2022-12-30 01:14:25 +0900

    Add doc-gen4.

--- lakefile.lean
+++ lakefile.lean
@@ -5,11 +5,10 @@ package dijkstra {
   -- add package configuration options here
 }
 
+@[default_target]
 lean_lib Dijkstra {
   -- add library configuration options here
 }
 
-@[default_target]
-lean_exe dijkstra {
-  root := `Main
-}
+meta if get_config? doc = some "on" then
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
