81543f27ea34811910c10871ecf7e6243f952b19
Junology <jtachan@hotmail.com>
2023-09-15 18:25:40 +0900

    chore: fix Std version

--- lakefile.lean
+++ lakefile.lean
@@ -14,4 +14,4 @@ meta if get_config? doc = some "on" then
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
 
 require «std» from git
-  "https://github.com/leanprover/std4" @ "main"
+  "https://github.com/leanprover/std4" @ "28459f72f3190b0f540b49ab769745819eeb1c5e"
